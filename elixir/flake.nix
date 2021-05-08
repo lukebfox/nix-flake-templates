6db6f074124167e27436133b9613df000c9cd8f2
Luke Bentley-Fox <mail@lukebentleyfox.net>
2021-05-08 17:13:35 +0100

    format

--- elixir/flake.nix
+++ elixir/flake.nix
@@ -2,10 +2,9 @@
   description = "Nix Flake Template | Elixir";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.utils.url = "github:numtide/flake-utils";
+  inputs.utils.url   = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, utils, ... }:
-    utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, utils, ... }: utils.lib.eachDefaultSystem (system:
     let
       inherit (nixpkgs.lib) optional;
       pkgs = import nixpkgs { inherit system; };
@@ -25,16 +24,12 @@
       #  rev = "eb069dd43ba98958f4161b070d111f952b1c656c";
       #  sha256 = "0kwqdy75x0xkld1gpzz355h9yw57c6jpq1b7lz7pkn5kxywxn9qb";
       #};
-    in
-      {
-
-          devShell = pkgs.mkShell {
-          buildInputs = [
-            elixir
-          ] ++ optional pkgs.stdenv.isLinux pkgs.libnotify # For ExUnit Notifier on Linux.
-            ++ optional pkgs.stdenv.isLinux pkgs.inotify-tools; # For file_system on Linux.
-        };
-      }
-  );
-
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = [
+          elixir
+        ] ++ optional pkgs.stdenv.isLinux pkgs.libnotify # For ExUnit Notifier on Linux.
+          ++ optional pkgs.stdenv.isLinux pkgs.inotify-tools; # For file_system on Linux.
+      };
+    });
 }
--- python/flake.nix
+++ python/flake.nix
@@ -4,36 +4,32 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.utils.url   = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, utils, ... }:
-    utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, utils, ... }: utils.lib.eachDefaultSystem (system:
     let
-      pkgs = import nixpkgs { inherit system;  };
+      pkgs = import nixpkgs { inherit system; };
 
       python = pkgs.python39;
       projectDir = ./.;
       overrides = pkgs.poetry2nix.overrides.withDefaults (import ./overrides.nix pkgs);
-
     in {
 
-      packages.${system} = {
+      devShell.${system} = pkgs.mkShell {
+        buildInputs = [
+          pkgs.python39Packages.poetry
+          (pkgs.poetry2nix.mkPoetryEnv {
+            inherit python projectDir overrides;
+          })
+        ];
+      };
 
+      packages.${system} = {
         package-name = pkgs.poetry2nix.mkPoetryApplication {
           inherit python projectDir overrides;
           # non-python runtime deps...
           propogatedBuildInputs = [];
         };
-
       };
 
-      devShell.${system} = pkgs.mkShell {
-        buildInputs = [
-          #(pkgs.poetry2nix.mkPoetryEnv {
-          #  inherit python projectDir overrides;
-          #})
-          pkgs.python39Packages.poetry
-        ];
-      };
-
-    };
+    });
 
 }
